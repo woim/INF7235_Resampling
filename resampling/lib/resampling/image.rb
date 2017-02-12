e87b2de2dccb7a73f1147771692eafc0ddb16a46
Élodie Marteau <elodiemarteau@MacBook-Pro-de-Elodie.local>
2017-02-12 02:26:45 -0500

    Adding interpolation to class Image

--- resampling/lib/resampling/image.rb
+++ resampling/lib/resampling/image.rb
@@ -1,3 +1,5 @@
+require 'matrix'
+
 class Image
   attr_reader :data
 
@@ -19,6 +21,47 @@ class Image
 
   def set_pixel( coord, color )
     @data.set_pixel( coord[0], coord[1], color)
+  end
+
+  def interpolate( coord )
+    return 0 if point_outside?( coord )
+    neighbors = get_neighbors( coord )
+    puts neighbors.to_s
+    puts neighbors.map { |e| e[0] }.to_s
+    ChunkyPNG::Color.rgb( bilinear_interpolation( coord, neighbors.map { |e| e[0] } ), \
+                          bilinear_interpolation( coord, neighbors.map { |e| e[1] } ), \
+                          bilinear_interpolation( coord, neighbors.map { |e| e[2] } ) )
+  end
+
+  private
+
+  def point_outside?( coord )
+    return true if( coord.any? { |e| e < 0 } )
+    return true if( coord[0] > @data.width )
+    return true if( coord[1] > @data.height )
+    false
+  end
+
+  def get_neighbors( coord )
+    integer_coord = coord.map { |e| e.to_int }
+    [ ChunkyPNG::Color.to_truecolor_bytes( @data.get_pixel( integer_coord[0], integer_coord[1] ) ),
+      ChunkyPNG::Color.to_truecolor_bytes( @data.get_pixel( integer_coord[0], integer_coord[1]+1 ) ),
+      ChunkyPNG::Color.to_truecolor_bytes( @data.get_pixel( integer_coord[0]+1, integer_coord[1] ) ),
+      ChunkyPNG::Color.to_truecolor_bytes( @data.get_pixel( integer_coord[0]+1, integer_coord[1]+1 ) ) ]
+  end
+
+  def bilinear_interpolation( coord, neighbors )
+    # wikipedia article on bilinear interpolation
+    # https://en.wikipedia.org/wiki/Bilinear_interpolation
+    integer_coord = coord.map { |e| e.to_int }
+    x = coord[0] - integer_coord[0]
+    y = coord[1] - integer_coord[1]
+    x_vector = Matrix[ [1-x, x] ]
+    neighbors_matrix = Matrix[  [neighbors[0], neighbors[1]], \
+                                [neighbors[2], neighbors[3]] ]
+    y_vector = Matrix[ [1-y, y] ].transpose
+    value = x_vector * neighbors_matrix * y_vector
+    value[0,0].to_int
   end
 
 end
--- resampling/test/test_image.rb
+++ resampling/test/test_image.rb
@@ -46,4 +46,23 @@ describe Image do
     end
   end
 
+  describe "#interpolate" do
+    it "interpolate non integer pixel" do
+      image = Image.new
+      image.load(filename)
+      color = image.interpolate( [0.5,0.5] )
+      image_expected = ChunkyPNG::Image.from_file(filename)
+      neighbor = [  ChunkyPNG::Color.to_truecolor_bytes( image_expected.get_pixel( 0, 0 ) ),
+                    ChunkyPNG::Color.to_truecolor_bytes( image_expected.get_pixel( 0, 1 ) ),
+                    ChunkyPNG::Color.to_truecolor_bytes( image_expected.get_pixel( 1, 0 ) ),
+                    ChunkyPNG::Color.to_truecolor_bytes( image_expected.get_pixel( 1, 1 ) ) ]
+      r = neighbor.map { |e| e[0] }.inject{ |sum, el| sum + el }.to_f / 4
+      g = neighbor.map { |e| e[1] }.inject{ |sum, el| sum + el }.to_f / 4
+      b = neighbor.map { |e| e[2] }.inject{ |sum, el| sum + el }.to_f / 4
+      color_expected = ChunkyPNG::Color.rgb( r.to_int, g.to_int, b.to_int )
+      color.must_equal(color_expected)
+
+    end
+  end
+
 end
