70dc438bf130c99e2f68a9ff794b174be2475d46
Élodie Marteau <elodiemarteau@MacBook-Pro-de-Elodie.local>
2017-02-12 13:21:14 -0500

    fixing require issues

--- resampling/lib/resampling.rb
+++ resampling/lib/resampling.rb
@@ -2,7 +2,5 @@ require 'resampling/version.rb'
 require 'resampling/image.rb'
 require 'resampling/resampler.rb'
 require 'resampling/transform.rb'
-require 'chunky_png'
-require 'matrix'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
