fdca16a11e38f6e3a726850a96dbfdf95320e78f
Élodie Marteau <elodiemarteau@MacBook-Pro-de-Elodie.local>
2017-02-12 10:57:22 -0500

    refactoring class trnasform

--- resampling/lib/resampling.rb
+++ resampling/lib/resampling.rb
@@ -2,5 +2,7 @@ require 'resampling/version.rb'
 require 'resampling/image.rb'
 require 'resampling/resampler.rb'
 require 'resampling/transform.rb'
+require 'chunky_png'
+require 'matrix'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
