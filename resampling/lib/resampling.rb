fb4ac669b90c183a37e4c986e1cb127c38e8ff6b
francois <francoishebert@hotmail.fr>
2017-02-19 10:12:32 -0500

    Modification to run application from command line

--- resampling/lib/resampling.rb
+++ resampling/lib/resampling.rb
@@ -2,5 +2,7 @@ require 'resampling/version.rb'
 require 'resampling/image.rb'
 require 'resampling/resampler.rb'
 require 'resampling/transform.rb'
+require 'matrix'
+require 'chunky_png'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
