65915ed02791c0f4fcff0a2fdacc03820229daef
Hebert <fe891094@japet.labunix.uqam.ca>
2017-02-26 20:37:32 -0500

    Adding peach

--- resampling/lib/resampling/resampler.rb
+++ resampling/lib/resampling/resampler.rb
@@ -18,39 +18,57 @@ class Resampler
   end
 
   def process_pcall( nb_threads = PRuby.nb_threads )
-	  @nb_threads = ( nb_threads > @image_destination.samples.size ) ? \
-                    @iage_destination.smaples.size : nb_threads  
-    define_slice_index
+	  set_nb_threads( nb_threads )
+    set_slices_index
     PRuby.pcall(	0...@nb_threads, 
 						     	lambda do |t|
                     index = @slice_index[t]
   	                samples = @image_destination.samples.slice( index[0],index[1] )
 	  								process( samples )
 	 	     					end
-		   				 )
+		   				 ) 
+  end
+
+  def process_peach( nb_threads = PRuby.nb_threads )
+    set_nb_threads( nb_threads )
+    image_destination.samples.peach( nb_threads: @nb_threads ) do |s|
+      interpolate( s )
+    end
   end
 
   def create_images
-    @image_source.load(@source_filename)
-    @image_destination.create_image( @image_source.data.width, @image_source.data.height )
+    @image_source.load( @source_filename ) 
+    @image_destination.create_image( 
+                            @image_source.data.width, 
+                            @image_source.data.height
+                                   )
   end
 
 	private 
 
 	def process( samples )
 		samples.each do |p|
-    	src_coord = @transform.transform_point(p)
-      @image_destination.set_pixel( p, @image_source.interpolate( src_coord ) )
+      interpolate( p )
     end 
 	end
 
-	def define_slice_index
-    @slice_index = []
- 	  remainder = @image_destination.samples.size % @nb_threads
-    length = @image_destination.samples.size / @nb_threads 
+  def interpolate( p )
+    src_coord = @transform.transform_point( p )
+    @image_destination.set_pixel( p, @image_source.interpolate( src_coord ) )
+  end
+
+  def set_nb_threads( nb_threads )
+    @nb_threads = ( nb_threads > @image_destination.samples.size ) ? \
+                      @iage_destination.smaples.size : nb_threads 
+    puts "Number of threads: " + @nb_threads.to_s     
+  end
+
+  def set_slices_index
+ 	  @slice_index = []
     start = 0
-    (0...@nb_threads).each do |t|
-		  size = length
+    remainder = @image_destination.samples.size % @nb_threads
+		(0...@nb_threads).each do |t|
+      size = @image_destination.samples.size / @nb_threads
       size += 1 if remainder > 0
       @slice_index.push( [start,size] )
       start += size
